6e4d2fbbb711375f9a3b77d1648d27ee111835f3
Ironicocos <131069303+Ironicocos@users.noreply.github.com>
2025-08-13 20:44:01 -0300

    Add files via upload

--- hogwarts.py
+++ hogwarts.py
@@ -2,13 +2,16 @@ import random
 hechizos_basicos = ["Lumos", "Alohomora", "Wingardium Leviosa"]
 atkSpells = ["Lumos", "Nox", "Alohomora", "Colloportus", "Wingardium Leviosa", "Accio", "Depulso", "Locomotor", "Scourgify", "Reparo", "Aparecium", "Rictusempra", "Tarantallegra", "Sonorus", "Quietus"]
 randomSpell = random.choice(atkSpells)
-def spellCast():
-    learntSpell = False
+learntSpell = False
+def spellCast(learntSpell):
+    print(learntSpell)
     print(f"El jugador castea {randomSpell}")
     for spell in hechizos_basicos:
         if spell == randomSpell:
-            learntSpell = True
-            return print("Hechizo casteado con éxito")
+            print("Hechizo casteado con éxito")
+            learntSpell = True 
+            break
     if learntSpell == False:
-        print("Aún no aprendiste ese hechizo")
-spellCast()
+            print("Aún no aprendiste ese hechizo")
+    return learntSpell
+learntSpell = spellCast(learntSpell)
